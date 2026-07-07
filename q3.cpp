f232045519b06403496cf35df934d4f79b21592c
pinkesh <pinkeshpatel9@yahoo.co.in>
2026-07-07 21:37:16 +0530

    made changed to q3 file

--- q3.cpp
+++ q3.cpp
@@ -1,9 +1,9 @@
 struct Stack {
     int*  data;   // heap-allocated array
     int   top;    // index of next free slot
-    int   cap;    // current capacity
+    int   cap;    // current capacity new line
 };
-
+//comment 1
 void  init   (Stack& s, int capacity){
    
 }
@@ -20,6 +20,6 @@ int   peek   (const Stack& s){
 
 }
 
-void  destroy(Stack& s){
-
+int  size   (const Stack& s){
+    return s.top;
 }
